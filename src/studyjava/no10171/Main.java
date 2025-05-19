765d92ea461f39da59923a9ceb34e139c75f3102
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-19 19:19:33 +0900

    solve no10171

---
+++ src/studyjava/no10171/Main.java
@@ -0,0 +1,13 @@
+package studyjava.no10171;
+
+// 아래의 고양이 모양 만들기
+// \    /\
+//  )  ( ')
+// (  /  )
+//  \(__)|
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+    }
+}

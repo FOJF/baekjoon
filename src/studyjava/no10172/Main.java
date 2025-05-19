749afb2967c9484807ba0f06841ba57b5d44e32f
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-19 19:23:02 +0900

    solve no10172

---
+++ src/studyjava/no10172/Main.java
@@ -0,0 +1,19 @@
+package studyjava.no10172;
+
+//아래의 강아지 그림 그리기
+//|\_/|
+//|q p|   /}
+//( 0 )"""\
+//|"^"`    |
+//||_/=\\__|
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}

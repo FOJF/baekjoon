e1f4cb89427e0fe12d96e1b9a77a6c69795e1c90
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-22 19:06:06 +0900

    주석 추가

--- src/studyjava/no30802/Main.java
+++ src/studyjava/no30802/Main.java
@@ -12,20 +12,23 @@ public class Main {
 
         int[] sizes = new int[strSizes.length];
 
-        int t = sc.nextInt();
-        int p = sc.nextInt();
+        int t = sc.nextInt(); // 티셔츠 구매시 한 묶음 당 들어있는 티셔츠의 수
+        int p = sc.nextInt(); // 펜 구매시 한 묶음 당 들어있는 펜의 수
 
-        for (int i = 0; i < strSizes.length; i++) {
+        for (int i = 0; i < strSizes.length; i++) { // 사이즈별 필요한 티셔츠 수
             sizes[i] = Integer.parseInt(strSizes[i]);
         }
 
         int buyT = 0;
-        for (int size : sizes) {
+        for (int size : sizes) { // 사이즈 별로 필요한 티셔츠 묶음 수 체크
             buyT += (size / t);
             if (size % t > 0)
                 buyT++;
         }
         System.out.println(buyT);
-        System.out.println(n / p + " " + n % p);
+        System.out.println((n / p) + " " + (n % p)); // 펜 몇 묶음 사야하고 한 개씩 사야하는 지
     }
 }
+
+// 티셔츠와 펜이 각각 한 묶음에 들어있는 양이 정해져 있고, 모자르면 안될때 티셔츠는 n 장씩 몇 묶음을 시켜야 하나
+// 펜은 딱 맞게 사려면 몇 묶음을 사고 한 개씩 몇개를 사야하냐

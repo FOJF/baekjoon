a6f35cb56ab8a4a1c6ef02e8adaac9badec8448f
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-22 14:47:01 +0900

    2675번 주석 추가

--- src/studyjava/no2675/Main.java
+++ src/studyjava/no2675/Main.java
@@ -8,17 +8,17 @@ public class Main {
 
         int testTimes = Integer.parseInt(sc.nextLine());
 
-        for(; testTimes > 0; testTimes--) {
+        for(; testTimes > 0; testTimes--) { // 테스트 횟수만큼 반복
             String[] testCase = sc.nextLine().split(" ");
-            int repeatTimes = Integer.parseInt(testCase[0]);
-            String str = testCase[1];
+            int repeatTimes = Integer.parseInt(testCase[0]); // 문자열을 반복할 횟수
+            String str = testCase[1]; // 반복할 문자열
 
-            for(int i = 0; i < str.length(); i++) {
-                for(int j = 0; j < repeatTimes; j++) {
-                    System.out.print(str.charAt(i));
+            for(int i = 0; i < str.length(); i++) { // 한 글자씩 순회
+                for(int j = 0; j < repeatTimes; j++) { // 반복할 횟수만큼 반복
+                    System.out.print(str.charAt(i)); // 개행 없이 출력
                 }
             }
-                System.out.println();
+                System.out.println(); // 다음 테스트를 위한 개행
         }
     }
 }

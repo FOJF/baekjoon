70f8e6b58a76453325e39026aa4b146ee7f1a102
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-23 20:35:10 +0900

    Create no1920.Main.java

---
+++ src/studyjava/no1920/Main.java
@@ -0,0 +1,27 @@
+package studyjava.no1920;
+
+import java.io.*;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.readLine(); // N
+        String[] A = br.readLine().split(" "); // 검색시 처음에 들어오는 수를 찾을 수 있도록 공백 추가
+        br.readLine(); // M
+        String[] strs = br.readLine().split(" ");
+        br.close();
+
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(String s : strs) {
+            A
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}

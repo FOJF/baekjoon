eeb9840c9acf2c8dca047b1154fffca56ae9dae7
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-23 11:16:34 +0900

    solve no2751

---
+++ src/studyjava/no2751/Main.java
@@ -0,0 +1,32 @@
+package studyjava.no2751;
+
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] intArray = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            intArray[i] = Integer.parseInt(br.readLine());
+            ;
+        }
+
+        br.close();
+
+        Arrays.sort(intArray);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i = 0; i < n; i++) {
+            bw.write(intArray[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+}

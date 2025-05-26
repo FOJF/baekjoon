1923b294e4a2251a7edeae31d1a90b6b815e32b6
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-26 16:02:19 +0900

    solve no11866

---
+++ src/studyjava/no11866/Main.java
@@ -0,0 +1,63 @@
+package studyjava.no11866;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        br.close();
+
+        int n = Integer.parseInt(s.split(" ")[0]);
+        int k = Integer.parseInt(s.split(" ")[1]);
+
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = i + 1;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("<");
+
+        int size = numbers.length;
+        int index = -1;
+
+        while (size > 1) {
+            int countNotEmpty = 0;
+
+            while (countNotEmpty < k) {
+                index = (index + 1) % numbers.length;
+
+                if (numbers[index] != 0) countNotEmpty++;
+
+                System.out.println("numbers[" + index + "] = " + numbers[index] + " " + countNotEmpty);
+            }
+            System.out.println("-------");
+
+            int removedNum = pop(numbers, index);
+            bw.write(removedNum + ", ");
+            size--;
+        }
+
+        for (int number : numbers) {
+            if (number != 0) {
+                bw.write(number + ">");
+                break;
+            }
+        }
+
+//        bw.write("\b\b>\n"); // 콘솔 출력 상황이 아니라면 예상한 것과 다르게 동작 가능함 -> 그래서 틀림
+        bw.flush();
+        bw.close();
+    }
+
+    static int pop(int[] targetArr, int index) {
+        int val = targetArr[index];
+        targetArr[index] = 0;
+        return val;
+    }
+}
+
+
+// \b는 모든 경우에 상상한 것처럼 작동 하지 않는다. 최대한 사용을 피해야할듯....

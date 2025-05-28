9a05133af5787984ce2dddc1509c87eb00581f30
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-28 21:20:01 +0900

    create no2579

---
+++ src/studyjava/no2579/Main.java
@@ -0,0 +1,26 @@
+package studyjava.no2579;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] points = new int[n];
+        for (int i = 0; i < n; i++) {
+            points[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+
+        int score = 0;
+
+        for (int i = 0; i < n; i++) {
+
+        }
+
+        System.out.println(score);
+    }
+}

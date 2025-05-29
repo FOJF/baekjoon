4e491329eaf73c4fb8ff078b06cfb414d40bc806
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-29 21:15:08 +0900

    create no9095

---
+++ src/studyjava/no9095/Main.java
@@ -0,0 +1,22 @@
+package studyjava.no9095;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        // int[] dp = new int[2];
+        int count = 0;
+        int a = 1;
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            for
+        }
+        br.close();
+    }
+}

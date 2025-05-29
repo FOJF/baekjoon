df4003e445ff047fb1fab992b8da761c3695ce31
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-29 11:05:49 +0900

    solve no2579

--- src/studyjava/no2579/Main.java
+++ src/studyjava/no2579/Main.java
@@ -10,17 +10,29 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] points = new int[n];
+        int[] maxPoints = new int[n];
+
         for (int i = 0; i < n; i++) {
             points[i] = Integer.parseInt(br.readLine());
         }
         br.close();
 
-        int score = 0;
-
         for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                maxPoints[i] = points[i];
+                continue;
+            } else if (i == 1) {
+                maxPoints[i] = points[i - 1] + points[i];
+                continue;
+            } else if (i == 2) {
+                maxPoints[i] = points[i] + Math.max(points[i - 2], points[i - 1]);
+                continue;
+            }
+
+            maxPoints[i] = points[i] + Math.max(maxPoints[i - 3] + points[i - 1], maxPoints[i - 2]);
 
         }
 
-        System.out.println(score);
+        System.out.println(maxPoints[n-1]);
     }
 }

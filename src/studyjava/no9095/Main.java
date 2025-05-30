1173c19d61dd04e780a84008bcd6968a55ab5a3d
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-30 09:06:24 +0900

    solve no9095

--- src/studyjava/no9095/Main.java
+++ src/studyjava/no9095/Main.java
@@ -1,21 +1,34 @@
 package studyjava.no9095;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        // int[] dp = new int[2];
-        int count = 0;
-        int a = 1;
+
 
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
+            int answer = 0;
+            int[] dp = {1, 1, 2, 3};
 
-            for
+            for (int j = 1; j <= n; j++) {
+                if (j <= 4) {
+                    answer += dp[j - 1];
+                } else {
+                    dp[0] = dp[1];
+                    dp[1] = dp[2];
+                    dp[2] = dp[3];
+
+                    int gapBetweenNowAndNext = dp[0] + dp[1] + dp[2];
+
+                    dp[3] = gapBetweenNowAndNext;
+
+                    answer += gapBetweenNowAndNext;
+                }
+            }
+            System.out.println(answer);
         }
         br.close();
     }

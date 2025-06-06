12c9a2406ccf2bc1276095b2ccbb40d86658b54b
FOJF <john9823@naver.com>
2025-06-06 09:55:26 +0900

    solve no2805

--- src/studyjava/no2805/Main.java
+++ src/studyjava/no2805/Main.java
@@ -1,45 +1,53 @@
+package studyjava.no2805;
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String[] nmStr = br.readLine().split(" ");
-		int n = Integer.parseInt(nmStr[0]);
-		int target = Integer.parseInt(nmStr[1]);
+        String[] nmStr = br.readLine().split(" ");
+        int n = Integer.parseInt(nmStr[0]);
+        int target = Integer.parseInt(nmStr[1]);
 
-		String[] treeHeightStrs = br.readLine().split(" ");
-		br.close();
+        String[] treeHeightStrs = br.readLine().split(" ");
+        br.close();
 
+        int[] trees = new int[n];
 
-		int[] trees = new int[n];
+        int start = 0;
+        int end = 0;
 
-		for(int i = 0; i < n; i++) {
-			trees[i] = Integer.parseInt(treeHeightStrs[i]);
-		}
+        for (int i = 0; i < n; i++) {
+            trees[i] = Integer.parseInt(treeHeightStrs[i]);
+            end = Math.max(end, trees[i] - 1);
+        }
 
-		Arrays.sort(trees);
+        start = Math.max(0, end - target);
 
-		int max = Math.max(0,trees[n-1]-target);
-		int testHeight = max;
+        int answer = 0;
 
-		while(true) {
-			if (isPossible(trees, testHeight, target)) 
-				max = testHeight++;
-			else break;
-		}
+        while (start <= end) {
+            int mid = (start + end) / 2;
 
-		System.out.println(max);
-	}
+            long sum = 0;
+            for (int tree : trees) {
+                if (tree > mid)
+                    sum += tree - mid;
+            }
 
-	public static boolean isPossible(int[] trees, int height, int target) {
-		int sum = 0;
-		for(int tree : trees) {
-			if(tree > height) {
-				sum += tree-height;
-			}
-		}
-		return sum >= target;
-	}
+            if (sum > target) {
+                start = mid + 1;
+                answer = mid;
+            } else if (sum == target) {
+                answer = mid;
+                break;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
 }

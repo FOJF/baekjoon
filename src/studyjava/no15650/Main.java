b09c74ac190a232229dd873ac97dea5761e31e10
fojf <john9823@naver.com>
2025-06-10 17:46:37 +0900

    change method name

--- src/studyjava/no15650/Main.java
+++ src/studyjava/no15650/Main.java
@@ -1,39 +1,37 @@
+package studyjava.no15650;
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n;
-	static int m;
-	static boolean[] isVisited;
+    static int n;
+    static int m;
+    static boolean[] isVisited;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String[] input = br.readLine().split(" ");
-		n = Integer.parseInt(input[0]);
-		m = Integer.parseInt(input[1]);
-		br.close();
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        br.close();
 
 
-		isVisited = new boolean[n];
+        isVisited = new boolean[n];
 
-		f(1, 1, "");
-	}
+        getAnswer(1, 1, "");
+    }
 
-	// static void f(int start, int depth) {
-	// 	String answer = "";
-	// }
+    static void getAnswer(int start, int depth, String answer) {
+        if (depth == m) {
+            for (int i = start; i <= n; i++) {
+                System.out.println(answer + i);
+            }
+            return;
+        }
 
-	static void f(int start, int depth, String answer) {
-		if (depth == m) {
-			for(int i = start; i <= n; i++) {
-				System.out.println(answer + i);
-			}
-			return;
-		}
-		
-		for(int i = start; i < n; i++) {
-			f(i+1, depth+1, answer + i + " ");
-		}
-	}
+        for (int i = start; i < n; i++) {
+            getAnswer(i + 1, depth + 1, answer + i + " ");
+        }
+    }
 }

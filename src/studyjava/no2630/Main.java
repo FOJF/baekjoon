5ca005a9933cb5ae311a54b5d0eb39c7ccf6298d
FOJF <john9823@naver.com>
2025-06-13 21:50:43 +0900

    solve no2630

--- src/studyjava/no2630/Main.java
+++ src/studyjava/no2630/Main.java
@@ -3,69 +3,42 @@ import java.util.*;
 
 public class Main {
 	static int[][] paper;
-	static HashSet<String> answer = new HashSet<>();
+	static int[] count = new int[2];
+
 	public static void main (String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		paper = new int[n][n];
+		int size = Integer.parseInt(br.readLine());
+		paper = new int[size][size];
 
-		for(int i = 0; i < n; i++) {
+		for(int i = 0; i < size; i++) {
 			String[] strArr = br.readLine().split(" ");
-			for(int j = 0; j < n; j++) {
+			for(int j = 0; j < size; j++) {
 				paper[i][j] = Integer.parseInt(strArr[j]);
 			}
 		}
 		br.close();
 
-		// System.out.println(checkSameColor(new int[1][1]));
-		checkSameColor(0,0,n);
-		int count0 = 0;
-		int count1 = 0;
+		countColorPapers(0,0,size);
 
-		for(String s : answer) {
-			if(s.endsWith("1")) count1++;
-			else count0++;
+		for(int answer : count) {
+			System.out.println(answer);
 		}
-
-		System.out.println(count0);
-		System.out.println(count1);
-
 	}
 
-	// public static int countColoredPapers(int[][] paper) {
-	// 	if(!checkSameColor(paper,0,0,paper.length)) {
-	// 		return checkSameColor(paper, startIdx, ) + checkSameColor(paper, , ) + checkSameColor(paper, , ) + checkSameColor(paper, , );
-	// 	}
-	// 	return 1;
-	// }
+	public static void countColorPapers(int x, int y, int searchArea) {
+		int firstColor = paper[x][y];
 
-	public static void checkSameColor(int idxI, int idxJ, int size) {
-		int firstColor = paper[idxI][idxJ];
-		StringBuilder sb = new StringBuilder();
-		sb.append(idxI).append(idxJ).append(size).append(firstColor);
-
-		System.out.println(sb.toString());
-		if(answer.contains(sb.toString())) {System.out.println("이미 해");return;}
-		
-		// 그냥 여기 로직이 잘못된 거 같음
-		for (int i = idxI; i < idxI+size; i++) {
-			for(int j = idxJ; j < idxJ+size; j++) {
-				if (paper[i][j] != firstColor) {
-					int newIdxI = (idxI+size)/2;
-					int newIdxJ = (idxJ+size)/2;
-					size /= 2;
-		System.out.println("실");
-					checkSameColor(idxI, idxJ, size);
-					checkSameColor(idxI, newIdxJ, size);
-					checkSameColor(newIdxI, idxJ, size);
-					checkSameColor(newIdxI, newIdxJ, size);
+		for (int i = x; i < x+searchArea; i++) {
+			for (int j = y; j < y+searchArea; j++) {
+				if(paper[i][j] != firstColor) { // 검사하는 로직이 뭐가 잘 못 된 거지? (x+searchArea)/2 가 아니라 x+searchArea/2가 맞는 거였네 너무 헷갈리고
+					countColorPapers(x,y,searchArea/2);
+					countColorPapers(x+searchArea/2,y,searchArea/2);
+					countColorPapers(x,y+searchArea/2,searchArea/2);
+					countColorPapers(x+searchArea/2,y+searchArea/2,searchArea/2);
 					return;
 				}
 			}
 		}
-
-		System.out.println("통과");
-
-		answer.add(sb.toString());
+		count[firstColor]++;
 	}
 }

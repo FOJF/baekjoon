add8d8405142c054fea31beff86fa900a5b86292
fojf <john9823@naver.com>
2025-06-20 17:06:49 +0900

    solve no15652

---
+++ src/studyjava/no15652/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] nm = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		br.close();
+
+		getAnswer(1,0,nm[0],nm[1], "");
+	}
+
+	public static void getAnswer(int start, int depth, int n, int m, String s) {
+		if (depth == m) {
+			System.out.println(s);
+			return;
+		}
+		for (int i = start; i <= n; i++) {
+			getAnswer(i, depth+1, n, m, s + i + " ");
+		}
+	}	
+}

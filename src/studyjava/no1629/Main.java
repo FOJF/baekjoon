3274aad8cf8926c38455972a00ce3f12c968ae00
fojf <john9823@naver.com>
2025-06-22 14:30:32 +0900

    solve no1629
    오버플로우 진짜 조심하자

--- src/studyjava/no1629/Main.java
+++ src/studyjava/no1629/Main.java
@@ -1,25 +1,28 @@
-package studyjava.no1629;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
 
-        int a = Integer.parseInt(input[0]);
-        int b = Integer.parseInt(input[1]);
-        int c = Integer.parseInt(input[2]);
+        long a = Integer.parseInt(input[0]);
+        long b = Integer.parseInt(input[1]);
+        long c = Integer.parseInt(input[2]);
 
         br.close();
 
-        long answer = 1;
-        for (int i = 0; i < b; i++) {
-            answer *= a;
-            answer %= c;
+        System.out.println(aPowbModc(a,b,c));
+    }
+    
+    public static long aPowbModc(long a, long b, long c) {
+        if (b < 2) return a%c;
+        
+        long bb = aPowbModc(a,b/2,c);
+        
+        if (b%2 == 0) {
+            return ((bb%c) * (bb%c)) % c;
+        } else {
+            return ((((bb%c)*(bb%c))%c)*(a%c))%c;
         }
-        System.out.println(answer);
     }
 }

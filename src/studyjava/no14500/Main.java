5c549b639c1a9f8d08ea05d6d42d526095109af5
fojf <john9823@naver.com>
2025-06-22 13:36:50 +0900

    solve no14500
    tried bfs but too slow
    so changed to dfs and add ㅗ shape checking logic

--- src/studyjava/no14500/Main.java
+++ src/studyjava/no14500/Main.java
@@ -2,97 +2,80 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Point {
-		public int x, y;
+    static int n;
+    static int m;
+    static int[][] board;
+    final static int TETRO_SIZE = 4;
 
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+    static boolean[][] isVisited;
+    static int max = 0;
+    static int sum = 0;
+    static int count = 0;
 
-	static int n;
-	static int m;
-	static int[][] board;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        isVisited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
 
-		board = new int[n][m];
+        br.close();
 
-		for (int i = 0; i < n; i++) {
-			board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		}
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sum += board[i][j];
+                count++;
+                isVisited[i][j] = true;
+                dfs(i, j);
+                isVisited[i][j] = false;
+                count--;
+                sum -= board[i][j];
+            }
+        }
 
-		br.close();
+        System.out.println(max);
+    }
 
-		int max = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				max = Math.max(max, bfs(new Point(i,j)));
-			}
-		}
+    public static void dfs(int x, int y) {
+        if (count == TETRO_SIZE) {
+            max = Math.max(max, sum);
+            return;
+        }
 
-		System.out.println(max);
-	}
+        int[][] delta = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-	// TODO : bfs로 하니까 되는 지 확인도 못하고 시간 초과 발생, dfs로 해봐야하나....?
-	// 그리고 ㅗ 모양도 추가적으로 검사해야함(bfs로 검사하고 대충 모양만들어서 추가로 검사해주려고 했는데)
-	// bfs자체만으로도 시간초과 발생해버림
-	// ->> dfs도전해봐야겠다 
-	public static int bfs(Point startP) {
-		int[][] delta = {{1,0},{-1,0},{0,1},{0,-1}};
-		boolean[][] isVisited = new boolean[n][m];
+        for (int[] dir : delta) {
+            int nextX = x + dir[0];
+            int nextY = y + dir[1];
 
-		Queue<Point> bfsQ = new LinkedList<>();
+            if ((0 <= nextX && nextX < n) && (0 <= nextY && nextY < m) && !isVisited[nextX][nextY]) {
+                if (count == 2) { // ㅗ
+                    sum += board[nextX][nextY];
+                    count++;
+                    isVisited[nextX][nextY] = true;
+                    dfs(x, y);
+                    isVisited[nextX][nextY] = false;
+                    count--;
+                    sum -= board[nextX][nextY];
+                }
 
-		bfsQ.add(startP);
-		isVisited[startP.x][startP.y] = true;
+                sum += board[nextX][nextY];
+                count++;
+                isVisited[nextX][nextY] = true;
+                dfs(nextX, nextY);
+                isVisited[nextX][nextY] = false;
+                count--;
+                sum -= board[nextX][nextY];
+            }
+        }
+    }
 
-		int depth = 0;
-		int sum = board[startP.x][startP.y];
-
-		// System.out.println("---------------------------------");
-
-		while(depth < 3) {
-			Point cur = bfsQ.poll();
-
-			int max = 0;
-			Point next = null;
-
-			for (int[] dir : delta) {
-				int nextX = cur.x + dir[0];
-				int nextY = cur.y + dir[1];
-
-				if ((0 <= nextX && nextX < n) && (0 <= nextY && nextY < m) && !isVisited[nextX][nextY] && max < board[nextX][nextY]) {
-					max = board[nextX][nextY];
-					next = new Point(nextX, nextY);
-				}
-			}
-
-			isVisited[next.x][next.y] = true;
-
- 			if (next == null) {
- 				bfsQ.add(cur);
- 				continue;
- 			}
-			
-			bfsQ.add(next);
-			sum += max;
-			// System.out.println(sum + " " + max + " " + next.x + " " + next.y);
-			depth++;
-		}
-
-		return sum;
-	}
+    public static boolean isValidIdx(int x, int y) {
+        return ;
+    }
 }
-
-/*
-3 3
-0 5 6
-6 4 0
-0 6 0
-*/

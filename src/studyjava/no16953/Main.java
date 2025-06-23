b4edec55ab27904a81d94159205fa4fc9daa61eb
fojf <john9823@naver.com>
2025-06-23 11:36:10 +0900

    solve no16953

---
+++ src/studyjava/no16953/Main.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static Map<Long, Long> visited = new HashMap<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long[] input = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+		br.close();
+
+		bfs(input[0], input[1]);
+
+		System.out.println(visited.getOrDefault(input[1],-1L));
+	}
+
+	public static void bfs(long start, long target) {
+		Queue<Long> bfsQ = new LinkedList<>();
+
+		bfsQ.add(start);
+		visited.put(start, 1L);
+
+		while(!bfsQ.isEmpty()) {
+			long now = bfsQ.poll();
+
+			long next = now*10+1;
+			if (next <= target && visited.get(next) == null) {
+				bfsQ.add(next);
+				visited.put(next, visited.get(now)+1);
+			}
+			next = now*2;
+			if (next <= target && visited.get(next) == null) {
+				bfsQ.add(next);
+				visited.put(next, visited.get(now)+1);
+			}
+		}
+	}
+}

f100ecf18fa83d71d66415fce6af81c710f62653
fojf <john9823@naver.com>
2025-06-30 17:53:56 +0900

    change map,set to list

--- src/studyjava/no11724/Main.java
+++ src/studyjava/no11724/Main.java
@@ -1,87 +1,65 @@
-package studyjava.no11724;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static HashMap<Integer, HashSet<Integer>> vertices = new HashMap<>();
-	static boolean[] isVisited;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static List<List<Integer>> vertices;
+    static boolean[] isVisited;
 
-		String[] ve = br.readLine().split(" ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int vertexCnt = Integer.parseInt(ve[0]);
-		int edgeCnt = Integer.parseInt(ve[1]);
+        String[] ve = br.readLine().split(" ");
 
-		isVisited = new boolean[vertexCnt];
+        int vertexCnt = Integer.parseInt(ve[0]);
+        int edgeCnt = Integer.parseInt(ve[1]);
 
-		for(int i = 0; i < edgeCnt; i++) {
-			String[] vertex = br.readLine().split(" ");
-			addEdge(Integer.parseInt(vertex[0]), Integer.parseInt(vertex[1]));
-		}
-		// System.out.println("finished ADD EDGE");
+        isVisited = new boolean[vertexCnt + 1];
 
-		br.close();
+        vertices = new ArrayList<>();
+        for (int i = 0; i < vertexCnt + 1; i++) {
+            vertices.add(new ArrayList<>());
+        }
 
-		System.out.println(countConnectedComponent());
+        for (int i = 0; i < edgeCnt; i++) {
+            String[] vertex = br.readLine().split(" ");
+            int a = Integer.parseInt(vertex[0]);
+            int b = Integer.parseInt(vertex[1]);
+            vertices.get(a).add(b);
+            vertices.get(b).add(a);
+        }
 
-	}
+        br.close();
 
-	public static int countConnectedComponent() {
-		int count = 0;
-		for(int i = 0; i < isVisited.length; i++) {
-			if(isVisited[i]) continue;
-			// System.out.println("Start dfs("+(i+1)+")");
+        System.out.println(countConnectedComponent());
 
-			if (dfs(i+1)) count++;
+    }
 
-			// System.out.println("Finished dfs("+(i+1)+")");
-		}
-		return count;
-	}
+    public static int countConnectedComponent() {
+        int count = 0;
+        for (int i = 1; i < isVisited.length; i++) {
+            if (isVisited[i]) continue;
+            bfs(i);
+            count++;
+        }
+        return count;
+    }
 
-	public static void addEdge(int vertexA, int vertexB) {
-		HashSet<Integer> hsA = vertices.get(vertexA);
-		HashSet<Integer> hsB = vertices.get(vertexB);
-		
-		if (hsA == null)
-			hsA = new HashSet<>(); 
-		if (hsB == null) 
-			hsB = new HashSet<>();
+    public static void bfs(int startVertex) {
+        Queue<Integer> q = new LinkedList<>();
 
-		hsA.add(vertexB);
-		hsB.add(vertexA);
+        q.add(startVertex);
+        isVisited[startVertex] = true;
 
-		vertices.put(vertexA, hsA);
-		vertices.put(vertexB, hsB);
-	}
+        while (!q.isEmpty()) {
+            int vertex = q.poll();
+            List<Integer> linkedVertices = vertices.get(vertex);
 
-	public static boolean dfs(int startVertex) {
-		if(isVisited[startVertex-1]) return false;
-		Queue<Integer> q = new LinkedList<>();
+            for (Integer linkedVertex : linkedVertices) {
+                if (isVisited[linkedVertex]) continue;
 
-		q.add(startVertex);
-		isVisited[startVertex-1] = true;
-
-		while(!q.isEmpty()) {
-			int vertex = q.poll();
-			// System.out.println("탐색 : "+vertex);
-			HashSet<Integer> linkedVertices = vertices.get(vertex);
-			if(linkedVertices == null) return true;
-
-			// System.out.println("linkedVertex");
-
-			for(Integer linkedVertex : linkedVertices) {
-			// System.out.println(linkedVertex);
-
-				if(isVisited[linkedVertex-1]) continue;
-
-				q.add(linkedVertex);
-				isVisited[linkedVertex-1] = true;
-			}
-		}
-
-		return true;
-	}
+                q.add(linkedVertex);
+                isVisited[linkedVertex] = true;
+            }
+        }
+    }
 }

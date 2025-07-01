fa5d1c4f5817ae764140d95de3b4b8bcc0384837
fojf <john9823@naver.com>
2025-07-01 10:17:31 +0900

    update

--- src/studyjava/no11725/Main.java
+++ src/studyjava/no11725/Main.java
@@ -2,64 +2,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static Map<Integer, Set<Integer>> graph;
+	static List<List<Integer>> adjList;
 	static int[] parents;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		graph = new HashMap<>();
+		adjList = new ArrayList<>();
 
 		int k = Integer.parseInt(br.readLine());
+        
+		for (int i = 0; i < k+1; i++) {
+            adjList.add(new ArrayList<>());
+        }
 
 		for (int i = 0; i < k-1; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			addEgde(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            adjList.get(a).add(b);
+            adjList.get(b).add(a);
 		}
 
 		br.close();
 
-		parents = new int[graph.size()-1];
+		parents = new int[k+1];
 
 		getParents();
 
 		StringBuilder sb = new StringBuilder();
 
-		for (int i : parents) {
-			sb.append(i).append("\n");
+		for (int i = 2; i < parents.length; i++) {
+			sb.append(parents[i]).append("\n");
 		}
 
 		System.out.println(sb);
 	}
 
-	public static void addEgde(int a, int b) {
-		Set<Integer> setA = graph.getOrDefault(a, new HashSet<>());
-		Set<Integer> setB = graph.getOrDefault(b, new HashSet<>());
-
-		setA.add(b);
-		setB.add(a);
-
-		graph.put(a, setA);
-		graph.put(b, setB);
-	}
-
 	public static void getParents() {
 		Queue<Integer> bfsQ = new LinkedList<>();
-		boolean[] isVisited = new boolean[graph.size()];
+		boolean[] isVisited = new boolean[adjList.size()];
 		
 		bfsQ.add(1);
-		isVisited[0] = true;
+		isVisited[1] = true;
 
 		while(!bfsQ.isEmpty()) {
 			int now = bfsQ.poll();
-			Set<Integer> linked = graph.get(now);
-
-			for (int next : linked) {
-				if (isVisited[next-1]) continue;
-
-				bfsQ.add(next);
-				isVisited[next-1] = true;
-				parents[next-2] = now;
-			}
+            
+            for (int next : adjList.get(now)) {
+                if (isVisited[next]) continue;
+                bfsQ.add(next);
+				isVisited[next] = true;
+                parents[next] = now;
+            }
 		}
 	}
 }

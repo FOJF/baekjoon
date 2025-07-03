8f7a7da24d6082629ca1bbf2a561fb5c2fc5d72b
fojf <john9823@naver.com>
2025-07-03 16:26:11 +0900

    solve no2230

---
+++ src/studyjava/no2230/Main.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[] numbers = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			numbers[i] = Integer.parseInt(br.readLine());
+		}
+
+		br.close();
+
+		Arrays.sort(numbers);
+
+		int s = 0;
+		int e = 0;
+
+		int answer = Integer.MAX_VALUE;
+
+		while(s <= e && e < n) {
+			int gap = numbers[e] - numbers[s];
+
+			if (gap == m) {
+				answer = gap;
+				break;
+			} else if (gap > m) {
+				answer = Math.min(answer, gap);
+				s++;
+			} else {
+				e++;
+			}
+		}
+
+		System.out.println(answer);
+	}
+}
